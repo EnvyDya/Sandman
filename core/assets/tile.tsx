452aea8897a3ba52a3b1cdf924ae9a9f40509e04
bondu <bondu@EnvyDya>
2022-02-16 12:39:17 +0100

    Bug tmx/tsx en local

--- core/assets/tile.tsx
+++ core/assets/tile.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.8" tiledversion="1.8.1" name="tile" tilewidth="16" tileheight="16" tilecount="84" columns="12">
- <image source="Forest Tileset/Tiles/TilesNonSliced.png" width="192" height="112"/>
+ <image source="TilesNonSliced.png" width="192" height="112"/>
 </tileset>
